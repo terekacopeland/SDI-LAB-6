7b80bbfe95e3db38e9db8f37d939de7624863581
terekacopeland <starsxmexico@live.com>
2014-03-15 13:59:03 -0400

    first

---
+++ copeland_tereka_lab6.js
@@ -0,0 +1,19 @@
+//alert("JavaScript works!");
+//SDI 1403
+//Lab 6
+//Working with arrays
+
+
+
+var walkingDeadCharacters = function() {
+	
+	var walkingDeadCharacters = ["Bob", "Glen", "Maggie", "Rick", "Michonne"];
+	var walkingDeadWeapons = ["knife", "bat", "hammer", "gun", "sword"];
+	
+	
+	
+	
+	
+	
+	
+}
